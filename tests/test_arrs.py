5fe393246385f4da633231f4451a24bd4a7f1904
Natalia Stroganova <ntstroganova@gmail.com>
2023-04-16 20:53:36 +0300

    create tests by unittest

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,14 @@
-from utils import arrs
+import unittest
+from utils.arrs import get
+from utils.arrs import my_slice
+
+class TestArrs(unittest.TestCase):
+    def test_get(self):
+        self.assertEqual(get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(get([], -1, "test"), "test")
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
-
-
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    def test_my_slice(self):
+        self.assertEqual(my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(my_slice([], 1), [])
